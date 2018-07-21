39ed35fc2d68e9c6e6741b2938accb20b660c32a
Jshen123 <joeshen123@gmail.com>
2018-07-21 01:34:49 -0700

    created folders and moved files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/client/client.js',
+  mode: process.env.NODE_ENV || 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
